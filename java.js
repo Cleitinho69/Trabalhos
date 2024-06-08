e31d7225a3b186890443c734f81185a7d22fb847
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2024-06-08 00:28:56 -0300

    .

--- java.js
+++ java.js
@@ -1,14 +1,29 @@
-
+/*Esse elemento serve apenas para o java checar se existe o elemento que ele quer mexer, tive que adicionar pq tava dando erro toda hora */
 document.addEventListener('DOMContentLoaded', function() {
     var bloco1 = document.getElementById('bloco_1');
     var bloco2 = document.getElementById('bloco_2');
+    var bloco3 = document.getElementById('bloco_3');
+    var bloco4 = document.getElementById('bloco_4');
+    var bloco5 = document.getElementById('bloco_5');
+    var bloco6 = document.getElementById('bloco_6');
+    var botão = document.getElementById('botão');
+
+    
     if (bloco1) {
-        bloco1.style.height = '620px';
-        bloco1.style.width = '300px';
-    }
-    if (bloco2){
-      bloco2.style.animation= "bottom 2s ease-in-out forwards";
-      bloco1.style.height = 'height: 620px'.width = '300px;';
+        /*Nessas duas eu coloquei a mesma animação, já que não muda muita coisa*/
+        bloco1.style.animation = "bloco1 2s ease-in-out forwards";
+        bloco3.style.animation = "bloco1 2s ease-in-out forwards";
+
+
+        bloco2.style.animation = "bloco2 2s ease-in-out 0.3s forwards";
+        bloco3.style.animation = "bloco1 2s ease-in-out forwards";
+        bloco4.style.animation = "bloco4 2s ease-in-out forwards";
+
+        /*Nessas duas eu coloquei a mesma animação, já que não muda muita coisa*/
+        bloco5.style.animation = "side 2s ease-in-out 1s forwards";
+        bloco6.style.animation = "side 2s ease-in-out 1s forwards";
+
+        botão.style.transition= '0.4s ease-in-out';
     }
   });
 
