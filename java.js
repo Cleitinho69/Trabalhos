24ebed2c646a5dfcbd1e2106406a4c1d8ea37250
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2024-07-04 11:12:39 -0300

    .

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ var bloco3 = document.getElementById("bloco_3");
 var bloco4 = document.getElementById("bloco_4");
 var bloco5 = document.getElementById("bloco_5");
 var bloco6 = document.getElementById("bloco_6");
-var botão = document.getElementById("botão");
+var botao = document.getElementById("botao");
 var pagina = document.getElementById("pagina");
 var h1 = document.getElementById("texto");
 
@@ -19,25 +19,48 @@ bloco4.style.animation = "bloco4 2s ease-in-out forwards";
 bloco5.style.animation = "side 2s ease-in-out 1s forwards";
 bloco6.style.animation = "side 2s ease-in-out 1s forwards";
 
-botão.style.transition = "2s ease-in-out";
 
-// botão.addEventListener("click", () => {
-//   botão.style.animation = "AniPagina 1s ease-in-out forwards";
+// Animação do botão no momento em que você clica
+function botao_enter(){
+  var chaves = document.querySelectorAll("#chaves, #direita, #esquerda");
 
-//   setTimeout(() => {
-//     botão.style.display = "none";
-//   }, 1000);
+  chaves[0].style.display = "flex";
 
-//   setTimeout(() => {
-//     pagina.style.display = "block";
-//   }, 1000);
+  setTimeout(()=>{
+    chaves[0].style.transition = "0.5s ease-in-out";
+    chaves[1].style.transition = "0.5s ease-in-out";
+    chaves[2].style.transition = "0.5s ease-in-out";
 
-//   setTimeout(() => {
-//     pagina.style.transform = "rotate(360deg)";
-//   }, 1100);
+    chaves[2].style.marginLeft = "115px";
+    chaves[1].style.marginRight = "115px";
+    chaves[0].style.marginLeft = "120px";
 
-// });
+    chaves[1].style.color = "white";
+    chaves[2].style.color = "white";
 
-setTimeout(()=>{
-  pagina.classList.add("show");
-},5000);
+    botao.style.transition = "0.5s ease-in-out";
+    botao.style.backgroundColor = "rgb(0, 174, 255)";
+    botao.style.color = "white";
+  },1);
+}
+
+function botao_out(){
+  var chaves = document.querySelectorAll("#chaves, #direita, #esquerda");
+
+  chaves[0].style.transition = "0.2s ease-in-out";
+  chaves[1].style.transition = "0.2s ease-in-out";
+  chaves[2].style.transition = "0.2s ease-in-out";
+
+  chaves[2].style.marginLeft = "0px";
+  chaves[1].style.marginRight = "0px";
+  chaves[0].style.marginLeft = "250px";
+
+  botao.style.backgroundColor = "transparent";
+  botao.style.color = "rgba(0, 0, 0, 0.308)";
+  chaves[1].style.color = "transparent";
+  chaves[2].style.color = "transparent";
+
+  setTimeout(()=>{
+    chaves[0].style.display = "none";
+  },200);
+}
